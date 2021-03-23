fd5678bd07bfcb9c68b1066593ebd0c089ec3b35
EC2 Default User <ec2-user@ip-172-31-36-129.ap-northeast-1.compute.internal>
2021-03-23 01:47:55 +0000

    first commit

---
+++ pictweet.rb
@@ -0,0 +1,44 @@
+require "bundler"
+
+Bundler.require
+
+set :database, {adapter: "sqlite3", database: "tweets.sqlite3"}
+
+class Tweet < ActiveRecord::Base
+end
+
+get "/" do
+  @tweets = Tweet.all
+  erb :index
+end
+
+get "/tweets/new" do
+  erb :new 
+end
+
+post "/tweets" do
+  Tweet.create(params)
+  redirect "/"
+end
+
+get "/tweets/:id/edit" do
+  @tweet = Tweet.find(params[:id])
+  erb :edit
+end
+
+patch "/tweets/:id" do
+  tweet = Tweet.find(params[:id])
+  tweet.update(text: params[:text], image: params[:image])
+  redirect "/"
+end
+
+get "/tweets/:id/delete" do
+  @tweet = Tweet.find(params[:id])
+  erb :delete
+end
+
+delete "/tweets/:id" do
+  tweet = Tweet.find(params[:id])
+  tweet.delete
+  redirect "/"
+end
